19cdf10ca1fc92d59b0fbcf99bc2abf381012b6f
Niclas Klugmann <niclas@klugmann.de>
2026-04-29 01:58:37 +0200

    remove sneaky space

--- sqlx-postgres/src/options/mod.rs
+++ sqlx-postgres/src/options/mod.rs
@@ -228,7 +228,7 @@ impl PgConnectOptions {
     }
 
     /// Enables the use of the `SSLKEYLOGFILE`` environment variable to export SSL session keys.
-    /// 
+    ///
     /// Only works with the `rustls` SSL backend
     pub fn ssl_enable_keylog(mut self, enable: bool) -> Self {
         self.ssl_enable_keylog = enable;
