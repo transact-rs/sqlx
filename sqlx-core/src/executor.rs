54b0d8b6fccaacb0df7761e3f58df61a691ebb77
Austin Bonander <austin.bonander@gmail.com>
2026-05-12 12:50:51 -0700

    chore: document `Executor` methods not for general use

--- sqlx-core/src/executor.rs
+++ sqlx-core/src/executor.rs
@@ -30,6 +30,26 @@ use std::{fmt::Debug, future};
 /// * `&mut transaction` -> `&mut *transaction`
 /// * `&mut connection` -> `&mut *connection`
 ///
+/// # Note: Methods Not for General Use
+/// This is a building-block trait that is exposed mainly for use as a trait bound.
+///
+/// Instead of calling the methods of this trait, use the free functions in the `sqlx` crate root:
+/// * [`sqlx::query()`]: use for DML queries (`SELECT`, `INSERT`, `UPDATE`, `DELETE`, etc.)
+///     * uses and caches prepared statements internally per-connection
+///     * may use argument placeholders (MySQL/MariaDB: `?`, Postgres/SQLite: `$1`, `$2`, etc.)
+///     * one query per string
+/// * [`sqlx::query_as()`], [`sqlx::query_scalar()`]: preferred for DML queries with type mapping
+/// * [`sqlx::raw_sql()`]: for DDL (`CREATE`, `ALTER`, etc.), batch queries and administration functions
+///     * accepts multiple queries separated by semicolons (`;`) in the same string
+///     * never uses prepared statements
+///
+/// Consider also the `query!()` family of macros, which offer queries and type mapping checked
+/// at compile-time.
+///
+/// [`sqlx::query()`]: crate::query::query
+/// [`sqlx::query_as()`]: crate::query_as::query_as
+/// [`sqlx::query_scalar()`]: crate::query_scalar::query_scalar
+/// [`sqlx::raw_sql()`]: crate::raw_sql::raw_sql
 pub trait Executor<'c>: Send + Debug + Sized {
     type Database: Database;
 
