54706f839c7966f1c361121ac4d25563770c4859
at264939-ctrl <at264939@gmail.com>
2026-06-27 19:41:22 +0300

    style: apply cargo fmt to time.rs

--- sqlx-sqlite/src/types/time.rs
+++ sqlx-sqlite/src/types/time.rs
@@ -1,14 +1,14 @@
 use crate::arguments::SqliteArgumentsBuffer;
 use crate::value::ValueRef;
 use crate::{
-    Sqlite, SqliteTypeInfo, SqliteValueRef,
     decode::Decode,
     encode::{Encode, IsNull},
     error::BoxDynError,
     type_info::DataType,
     types::Type,
+    Sqlite, SqliteTypeInfo, SqliteValueRef,
 };
-use time::format_description::{BorrowedFormatItem, well_known::Rfc3339};
+use time::format_description::{well_known::Rfc3339, BorrowedFormatItem};
 use time::macros::format_description as fd;
 use time::{Date, OffsetDateTime, PrimitiveDateTime, Time};
 
@@ -195,7 +195,7 @@ fn decode_datetime_from_text(value: &str) -> Option<PrimitiveDateTime> {
 #[allow(deprecated)]
 mod formats {
     use time::format_description::BorrowedFormatItem::{Component, Literal, Optional};
-    use time::format_description::{BorrowedFormatItem, Component::*, modifier};
+    use time::format_description::{modifier, BorrowedFormatItem, Component::*};
 
     const YEAR: BorrowedFormatItem<'_> = Component(Year({
         let mut value = modifier::Year::default();
